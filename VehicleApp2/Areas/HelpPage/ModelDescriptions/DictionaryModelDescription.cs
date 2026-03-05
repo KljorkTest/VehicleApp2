ace1782f72b0eb2f475ded6ccfc24c7878955fb6
Kljork <civcijad@yahoo.com>
2026-03-05 12:49:09 +0100

    Pocetna struktura rjesenja

---
+++ VehicleApp2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace VehicleApp2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ VehicleApp2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VehicleApp2.WebApiApplication" Language="C#" %>
