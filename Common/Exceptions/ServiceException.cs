df2141106dd4893138d3c336557605550b92e066
Kljork <civcijad@yahoo.com>
2026-03-13 12:56:29 +0100

    Custom Exception
    Napravljeni custom exceptioni

---
+++ Common/Exceptions/ServiceException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Exceptions
+{
+    public class ServiceException : Exception
+    {
+        public ServiceException(string message, Exception innerException) 
+            : base(message, innerException) 
+        { }
+    }
+}
