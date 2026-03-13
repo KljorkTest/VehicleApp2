8e79a9670f47275954d3ffe923abc22866620710
Kljork <civcijad@yahoo.com>
2026-03-13 13:50:36 +0100

    rjeseni stash problemi

---
+++ VehicleApp.Repository/VehicleMakeRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleApp.DAL;
+using VehicleApp.Repository.Common;
+using VehicleApp.Common;
+using System.Data.Entity;
+using Common.Exceptions;
+
+namespace VehicleApp.Repository
+{
+    public class VehicleMakeRepository : GenericRepository<VehicleMakeEntity>, IVehicleMakeRepository
+    {
+        public VehicleMakeRepository(VehicleDbContext context)
+            : base(context)
+        { }
+
+        public async Task<IEnumerable<VehicleMakeEntity>> GetAsync(
+            Paging paging,
+            Filtering filtering,
+            Sorting sorting)
+        {
+            IQueryable<VehicleMakeEntity> query = _dbSet;
+
+            if (filtering != null && !string.IsNullOrEmpty(filtering.Search))
+            {
+                query = query.Where(x => x.Name.Contains(filtering.Search) || x.Abrv.Contains(filtering.Search));
+            }
+
+            if (sorting != null && !string.IsNullOrEmpty(sorting.SortBy))
+            {
+                if (sorting.SortBy.Equals("Name", StringComparison.OrdinalIgnoreCase))
+                    query = sorting.IsDescending ? query.OrderByDescending(x => x.Name) : query.OrderBy(x => x.Name);
+
+                if (sorting.SortBy.Equals("Abrv", StringComparison.OrdinalIgnoreCase))
+                    query = sorting.IsDescending ? query.OrderByDescending(x => x.Abrv) : query.OrderBy(x => x.Abrv);
+            }
+            else
+            {
+                query = query.OrderBy(x => x.Name);
+            }
+
+            query = query
+                .Skip((paging.Page -1)  * paging.PageSize)
+                .Take(paging.PageSize);
+
+            try
+            {
+                return await query.ToListAsync();
+            }
+            catch(Exception ex)
+            {
+                throw new RepositoryException("Failed to load vehicle makes!", ex);
+            }
+        }
+    }
+}
---
+++ VehicleApp.Repository/VehicleModelRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleApp.Common;
+using VehicleApp.DAL;
+using VehicleApp.Repository.Common;
+using System.Data.Entity;
+
+namespace VehicleApp.Repository
+{
+    public class VehicleModelRepository : GenericRepository<VehicleModelEntity>, IVehicleModelRepository
+    {
+        public VehicleModelRepository(VehicleDbContext context) 
+            : base(context) 
+        { }
+
+        public async Task<IEnumerable<VehicleModelEntity>> GetAsync(
+            Guid? vehicleMakeId,
+            Paging paging,
+            Filtering filtering,
+            Sorting sorting)
+        {
+            IQueryable<VehicleModelEntity> query = _dbSet.Include(x => x.VehicleMake);
+
+            if (vehicleMakeId.HasValue)
+            {
+                query = query.Where(x => x.VehicleMakeId == vehicleMakeId.Value);
+            }
+        }
+    }
+}
