f9518a32d234fb2e85e05ad3dd2233b4e2bdc56b
Kljork <civcijad@yahoo.com>
2026-03-14 13:55:01 +0100

    Uow
    Unit of work i njegov interface napravljen

---
+++ VehicleApp.Repository.Common/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleApp.Repository.Common
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IVehicleMakeRepository VehicleMakes {  get; }
+        IVehicleModelRepository VehicleModels { get; }
+        IUserVehicleRepository UserVehicles { get; }
+        IUserRepository Users { get; }
+
+        Task SaveAsync();
+    }
+}
---
+++ VehicleApp.Repository/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleApp.DAL;
+using VehicleApp.Repository.Common;
+
+namespace VehicleApp.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly VehicleDbContext _context;
+
+        public IVehicleMakeRepository VehicleMakes { get; }
+        public IVehicleModelRepository VehicleModels {  get; }
+        public IUserVehicleRepository UserVehicles { get; }
+        public IUserRepository Users { get; }
+
+        public UnitOfWork (VehicleDbContext context)
+        {
+            _context = context;
+
+            VehicleMakes = new VehicleMakeRepository(_context);
+            VehicleModels = new VehicleModelRepository(_context);
+            UserVehicles = new UserVehicleRepository(_context);
+            Users = new UserRepository(_context);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
