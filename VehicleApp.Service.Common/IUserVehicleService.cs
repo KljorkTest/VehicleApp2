f1cc96c3bf71e76ba8b5ce137b63d5c5ddc67051
Kljork <civcijad@yahoo.com>
2026-06-04 12:27:35 +0200

    F

---
+++ VehicleApp.Service.Common/IUserVehicleService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleApp.Model;
+using VehicleApp.Common;
+using VehicleApp.Model.Common;
+
+namespace VehicleApp.Service.Common
+{
+    public interface IUserVehicleService
+    {
+        Task<IEnumerable<UserVehicle>> GetByUserIdAsync(
+            Paging paging,
+            Filtering filtering,
+            Sorting sorting,
+            Guid userId);
+
+        Task<UserVehicle> GetByIdAsync(Guid userId, Guid id);
+        Task CreateAsync(Guid userId, Guid vehicleModelId);
+        Task UpdateAsync(Guid userId, Guid id, Guid vehicleModelId);
+        Task DeleteAsync(Guid id);
+
+    }
+}
