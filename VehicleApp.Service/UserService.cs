edec35722018e12d283d82cfa257ae6490fcb722
Kljork <civcijad@yahoo.com>
2026-06-08 13:52:43 +0200

    User service napravljen

---
+++ VehicleApp.Service/UserService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleApp.Common;
+using VehicleApp.DAL;
+using VehicleApp.Model;
+using VehicleApp.Repository.Common;
+using VehicleApp.Service.Common;
+
+namespace VehicleApp.Service
+{
+    public class UserService : GenericService<UserEntity, User> , IUserService
+    {
+        private readonly IUnitOfWork _uow;
+
+        protected override IGenericRepository<UserEntity> Repository => _uow.Users;
+
+        public UserService(IUnitOfWork uow, IMapper mapper) : base(uow, mapper)
+        {
+        }
+
+        public async Task<IEnumerable<User>> GetAsync(
+            Paging paging,
+            Filtering filtering,
+            Sorting sorting)
+        {
+            var entities = await _uow.Users.GetAsync(paging, filtering, sorting);
+            return Mapper.Map<IEnumerable<User>>(entities);
+        }
+    }
+}
